7eed61a37bd29c7216c908066ecb11d70217a974
schwa <itkw456@gmail.com>
2024-12-09 01:12:21 +0900

    working on drawing box plot with variable names on their corresponding mean_variance on y axis

---
+++ finalterm/fianl_term/output_histogram.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+FILTER_IDX = None
+Y_PATH = '../data/dataset/dataset/y_train.csv'
+QUANT_VAL = 0.9999
+
+def get_output():
+    file_path = Y_PATH
+    y = pd.read_csv(file_path)
+
+    return y
+
+def get_histogram(data, outlier_boundary):
+    data.plot.hist(bins=100, alpha=0.7, edgecolor='black')
+
+    plt.title('Output Variable (y)')
+    plt.xlabel('Test Times')
+    plt.ylabel('Number of Data Points')
+
+    plt.axvline(outlier_boundary, linestyle='--', color='red', linewidth=2, label='Outlier Boundary')
+    plt.text(outlier_boundary, plt.gca().get_ylim()[1] * 0.9, f'outlier boundary: {outlier_boundary :.2f}  ',
+             color='black', rotation=0, ha='right', va='center', fontsize=10)
+
+    plt.show()
+
+def get_outlier(data):
+    outlier = data.quantile(QUANT_VAL).item()
+    print(f"{QUANT_VAL * 100} percentile 값: {outlier :.2f}")
+    return outlier
+
+def get_number_of_outliers(y, outlier_boundary):
+    outliers = y[y >= outlier_boundary]
+    outliers = outliers.dropna()
+    print(f'Number of Outliers: {len(outliers)}')
+    print(f'Outlier Index: {outliers['y'].index.item()}')
+    print(f'Outlier Value: {outliers['y'].item()}')
+    return outliers
+
+def get_filtered_y(y, outliers):
+    filtered_y = y[~y.isin(outliers['y'])]
+    return filtered_y
+
+def get_filtering_idx(outliers):
+    FILTER_IDX = outliers.index.item()
+    return FILTER_IDX
+
+def __main__():
+    y = get_output()
+    outlier_boundary = get_outlier(y)
+    get_histogram(y, outlier_boundary)
+    outliers = get_number_of_outliers(y, outlier_boundary)
+    filtered_y = get_filtered_y(y, outliers)
+
+
+if __name__ == '__main__':
+    __main__()
---
+++ finalterm/fianl_term/small_dist_reduction.py
@@ -0,0 +1,83 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from output_histogram import *
+
+X_TRAIN_PATH = '../data/dataset/dataset/X_train.csv'
+
+def get_X_data():
+    file_path = X_TRAIN_PATH
+    X_data = pd.read_csv(file_path)
+
+    return X_data
+
+def get_filtered_X_data(X_data, filtering_idx):
+    filtered_X_data = X_data.drop(index=filtering_idx)
+    return filtered_X_data
+"""
+def get_cat_with_y_df(X_data, y):
+    cat_vars = X_data.columns.tolist()[1:9]
+    combined_datas = []
+    for cat in cat_vars:
+        combined = pd.merge(X_data[cat], y, left_index=True, right_index=True)
+        combined_datas.append(combined)
+        print(combined)
+    return combined_datas
+"""
+
+def get_variance_df(X_data, y):
+    cat_vars = X_data.columns.tolist()[1:9]
+    combined_datas = []
+    variance_for_var = {}
+    for var in cat_vars:
+        merged = pd.merge(X_data[var], y, left_index=True, right_index=True)
+        combined = merged.groupby(var, as_index=False).agg({'y': 'mean'})
+        combined_datas.append(combined)
+        variance_for_var[var] = merged.groupby(var)['y'].var().mean().item()
+    variance_df = pd.DataFrame.from_dict(variance_for_var, orient='index')
+    variance_df = variance_df.rename(columns={0 : 'mean_variance'})
+    variance_df = variance_df.reset_index()
+    print(variance_df)
+    return variance_df
+
+
+def get_cat_box_plot(variance_df):
+    variance_df.boxplot()
+
+"""
+    positions = range(1, len(variance_df['mean_variance'].unique()) + 1)
+
+    for pos, var in zip(positions, variance_df['mean_variance'].unique()):
+        plt.text(pos, variance_df['mean_variance'].max() * 1.05, var, ha='center', va='center', fontsize=10, fontweight='bold')
+
+    plt.show()
+"""
+
+def cat_var_reduction(X_data, y):
+    cat_vars = X_data.columns.tolist()[1:9]
+
+    combined_data = pd.merge(X_data[cat_vars], y, left_index=True, right_index=True)
+
+
+def get_binary_vars(X_data):
+    binary_vars = X_data.columns.tolist()[10:]
+    return binary_vars
+
+def __main__():
+    y = get_output()
+    outlier_boundary = get_outlier(y)
+    outliers = get_number_of_outliers(y, outlier_boundary)
+    filtering_idx = get_filtering_idx(outliers)
+    y = get_filtered_y(y, outliers)
+
+    X_data = get_X_data()
+    filtered_X_data = get_filtered_X_data(X_data, filtering_idx)
+
+    variance_df = get_variance_df(filtered_X_data, y)
+
+    get_cat_box_plot(variance_df)
+
+    cat_var_reduction(filtered_X_data, y)
+
+if __name__ == '__main__':
+    __main__()
