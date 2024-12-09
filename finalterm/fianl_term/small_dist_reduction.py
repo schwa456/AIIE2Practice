ce83acfb58cbb29f119041c23dde07b93acc9728
schwa456 <itkw456@gmail.com>
2024-12-09 12:31:42 +0900

    ignore midterm, and reduction completed

--- finalterm/fianl_term/small_dist_reduction.py
+++ finalterm/fianl_term/small_dist_reduction.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-from .output_histogram import *
+from output_histogram import *
 
 X_TRAIN_PATH = '../data/dataset/dataset/X_train.csv'
 
@@ -14,19 +14,15 @@ def get_X_data():
 def get_filtered_X_data(X_data, filtering_idx):
     filtered_X_data = X_data.drop(index=filtering_idx)
     return filtered_X_data
-"""
-def get_cat_with_y_df(X_data, y):
-    cat_vars = X_data.columns.tolist()[1:9]
-    combined_datas = []
-    for cat in cat_vars:
-        combined = pd.merge(X_data[cat], y, left_index=True, right_index=True)
-        combined_datas.append(combined)
-        print(combined)
-    return combined_datas
-"""
 
-def get_variance_df(X_data, y):
+#######################################################
+
+def get_cat_vars(X_data):
     cat_vars = X_data.columns.tolist()[1:9]
+    return cat_vars
+
+def get_cat_variance_df(X_data, y):
+    cat_vars = get_cat_vars(X_data)
     combined_datas = []
     variance_for_var = {}
     for var in cat_vars:
@@ -37,31 +33,126 @@ def get_variance_df(X_data, y):
     variance_df = pd.DataFrame.from_dict(variance_for_var, orient='index')
     variance_df = variance_df.rename(columns={0 : 'mean_variance'})
     variance_df = variance_df.reset_index()
-    print(variance_df)
     return variance_df
+
+def get_cat_reducible_vars(variance_df):
+    q1_values = variance_df['mean_variance'].quantile(0.25)
+    below_q1 = variance_df[variance_df['mean_variance'] <= q1_values]['index']
+    below_q1 = below_q1.tolist()
+    print(f'Number of Categorical Variables that will be reduced : {len(below_q1)}')
+    print(f'Reduction Categorical Variable of {below_q1}')
+    return below_q1
 
 
 def get_cat_box_plot(variance_df):
     variance_df.boxplot()
 
-"""
-    positions = range(1, len(variance_df['mean_variance'].unique()) + 1)
+    box_width = 0.15
+    box_center = 1
 
-    for pos, var in zip(positions, variance_df['mean_variance'].unique()):
-        plt.text(pos, variance_df['mean_variance'].max() * 1.05, var, ha='center', va='center', fontsize=10, fontweight='bold')
+    mean_mean_variance = variance_df.mean(numeric_only=True).item()
+
+    median_mean_variance = variance_df.median(numeric_only=True).item()
+
+    for i, row in variance_df.iterrows():
+        plt.hlines(y=row['mean_variance'], xmin=box_center - box_width / 2, xmax=box_center + box_width / 2,
+                  color='red', linestyle='--', linewidth=1)
+        plt.text(x=box_center + box_width, y=row['mean_variance'],
+                 s=f"{row['index']} ({row['mean_variance']:.2f})",
+                 color='black', va='center', fontsize=10)
+
+    plt.hlines(y=mean_mean_variance, xmin=box_center - box_width / 2,
+               xmax=box_center + box_width / 2, color='blue', linestyle='--', linewidth=1)
+    plt.text(x=box_center - box_width, y=mean_mean_variance,
+             s=f"Mean ({mean_mean_variance:.2f})",
+             color="black", va='center', ha='right', fontsize=10)
+
+    plt.text(x=box_center - box_width, y=median_mean_variance,
+             s=f"Median ({median_mean_variance:.2f})",
+             color="black", va='center', ha='right', fontsize=10)
+
+    plt.title('Mean Variance of Categorical Variables', fontsize=10)
 
     plt.show()
-"""
 
-def cat_var_reduction(X_data, y):
-    cat_vars = X_data.columns.tolist()[1:9]
 
-    combined_data = pd.merge(X_data[cat_vars], y, left_index=True, right_index=True)
+def cat_variable_reduction(X_data, y):
+    variance_df = get_cat_variance_df(X_data, y)
 
+    get_cat_box_plot(variance_df)
+
+    reducible_variable = get_cat_reducible_vars(variance_df)
+
+    return reducible_variable
+
+################################################
 
 def get_binary_vars(X_data):
-    binary_vars = X_data.columns.tolist()[10:]
+    binary_vars = X_data.columns.tolist()[9:]
     return binary_vars
+
+def get_binary_variance_df(X_data):
+    bin_vars = get_binary_vars(X_data)
+    result = {}
+    for var in bin_vars:
+        result[var] = X_data[var].var()
+    variance_df = pd.DataFrame.from_dict(result, orient='index')
+    variance_df = variance_df.rename(columns={0 : 'variance'})
+    variance_df = variance_df.reset_index()
+    return variance_df
+
+def get_bin_scatter_plot(variance_df):
+    variance_df = variance_df.copy()
+    variance_df['index'] = variance_df['index'].apply(lambda x: x.replace('X', ''))
+
+    variance_df.plot.scatter(x='index', y='variance')
+
+
+    plt.xlabel('Binary Features')
+    plt.ylabel('Variance')
+    plt.title('Variance of Binary Variables')
+    plt.xticks([])
+
+    plt.axhline(y=0, color='red', linestyle='--', linewidth=1)
+
+    plt.show()
+
+def get_zero_variance(variance_df):
+    zero_df = variance_df[variance_df['variance'] == 0]
+    return zero_df
+
+def get_duplicated_variance(variance_df):
+    duplicated_df = variance_df[variance_df['variance'].duplicated(keep=False)]
+    return duplicated_df
+
+
+def bin_variable_reduction(X_data):
+    variance_df = get_binary_variance_df(X_data)
+
+    get_bin_scatter_plot(variance_df)
+
+    zero_df = get_zero_variance(variance_df)
+    zero_idx = zero_df['index'].tolist()
+
+    duplicated_df = get_duplicated_variance(variance_df)
+    duplicated_idx = duplicated_df['index'].tolist()
+
+    red_idx = zero_idx + duplicated_idx
+    red_idx = pd.Series(red_idx).drop_duplicates().tolist()
+
+    print(f'Number of Binary Variables that will be Reduced : {len(red_idx)}')
+
+    return red_idx
+
+################################################
+
+def get_reduced_X(X_data, reducible_vars):
+    X_reduced = X_data.drop(columns=reducible_vars)
+    print(f'Number of Stayed Variables : {X_reduced.shape[1]}')
+    return X_reduced
+
+
+################################################
 
 def __main__():
     y = get_output()
@@ -73,11 +164,16 @@ def __main__():
     X_data = get_X_data()
     filtered_X_data = get_filtered_X_data(X_data, filtering_idx)
 
-    variance_df = get_variance_df(filtered_X_data, y)
 
-    get_cat_box_plot(variance_df)
+    reducible_vars = []
+    cat_red_var = cat_variable_reduction(filtered_X_data, y)
+    reducible_vars.extend(cat_red_var)
+    ########################
+    bin_red_var = bin_variable_reduction(filtered_X_data)
+    reducible_vars.extend(bin_red_var)
 
-    cat_var_reduction(filtered_X_data, y)
+    X_reduced = get_reduced_X(filtered_X_data, reducible_vars)
+
 
 if __name__ == '__main__':
     __main__()
